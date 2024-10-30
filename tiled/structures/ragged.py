695ac496be937d447bbaffe7b3ea0237290eb01a
Dan Allan <dallan@bnl.gov>
2024-10-30 15:24:27 -0400

    Implement RaggedStructure.from_array

--- tiled/structures/ragged.py
+++ tiled/structures/ragged.py
@@ -1,8 +1,34 @@
 from typing import Tuple
 
-from .array import ArrayStructure
+from .array import ArrayStructure, BuiltinDtype, StructDtype
 
 
 class RaggedStructure(ArrayStructure):
     # None means "variable length".
     shape: Tuple[int | None, ...]
+
+    @classmethod
+    def from_array(cls, array, shape=None, chunks=None, dims=None) -> "RaggedStructure":
+        # Why would shape ever be different from array.shape, you ask?
+        # Some formats (notably Zarr) force shape to be a multiple of
+        # a chunk size, such that array.shape may include a margin beyond the
+        # actual data.
+        if shape is None:
+            shape = array.shape
+        # if chunks is None:
+        #     if hasattr(array, "chunks"):
+        #         chunks = array.chunks  # might be None
+        #     else:
+        #         chunks = None
+        #     if chunks is None:
+        #         chunks = ("auto",) * len(shape)
+        # normalized_chunks = normalize_chunks(
+        #     chunks,
+        #     shape=shape,
+        #     dtype=array.dtype,
+        # )
+        if array.dtype.fields is not None:
+            data_type = StructDtype.from_numpy_dtype(array.dtype)
+        else:
+            data_type = BuiltinDtype.from_numpy_dtype(array.dtype)
+        return cls(data_type=data_type, shape=shape, chunks=chunks, dims=dims)
